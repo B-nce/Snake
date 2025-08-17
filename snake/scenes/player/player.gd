8b4d77ca98881b7b3f7c63a01909bfb60037f846
B-nce <bokorbence1@gmail.com>
2025-08-17 10:04:57 +0200

    BodyParts node contains body parts as children

--- snake/scenes/player/player.gd
+++ snake/scenes/player/player.gd
@@ -6,19 +6,13 @@ const SnakeSpriteTypes = preload("res://scenes/player/snake_sprite_types.gd")
 @export var direction: float = Constants.ROTATION_UP
 @export var health_points: int = 1
 @export var sprite_file_path: String
-var _body_parts: Array[BodyPart] = []
 
   
 func initialize(snake_sprite_path: String) -> void:
 	self.sprite_file_path = snake_sprite_path
-	
 	var tail_postion: Vector2 = Vector2(position.x, position.y + Constants.SPRITE_SIZE)
-	var head: BodyPart = BodyPart.new(snake_sprite_path, position, SnakeSpriteTypes.Type.HEAD)
-	var tail: BodyPart = BodyPart.new(snake_sprite_path, tail_postion, SnakeSpriteTypes.Type.TAIL)
-	self.add_child(head)
-	self.add_child(tail) #TODO invent something better
-	self._body_parts.append(head)
-	self._body_parts.append(tail)
+	_add_body_part(position, SnakeSpriteTypes.Type.HEAD, false)
+	_add_body_part(tail_postion, SnakeSpriteTypes.Type.TAIL)
 
 
 func _input(event: InputEvent) -> void:
@@ -40,9 +34,22 @@ func _input(event: InputEvent) -> void:
 			move()
 
 
+func _add_body_part(part_position: Vector2, type: SnakeSpriteTypes.Type, with_collision: bool = true) -> void:
+	var new_part: BodyPart = BodyPart.new(sprite_file_path, part_position, type)
+	$BodyParts.add_child(new_part)
+	
+	if(with_collision):
+		var shape: RectangleShape2D = RectangleShape2D.new()
+		shape.size = Vector2(Constants.SPRITE_SIZE, Constants.SPRITE_SIZE)
+		var collision: CollisionShape2D = CollisionShape2D.new()
+		collision.shape = shape
+		new_part.add_child(collision)
+
 
 func move() -> void:
-	#rotation_degrees = rot
+	#first move the snake
+	#then add body part
+	#then set new sprite types and rotations
 	move_and_slide()
 	
 
