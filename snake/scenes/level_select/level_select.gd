81b38470e6b0acde5a34a37fcfae99722e20e5a6
Dominik-Bako <bdominik12864@gmail.com>
2025-09-11 14:59:43 +0200

    level select fix

--- snake/scenes/level_select/level_select.gd
+++ snake/scenes/level_select/level_select.gd
@@ -5,3 +5,7 @@ func _on_button_pressed() -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu_scene/main_menu.tscn")
+
+
+func _on_level_1_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1/level_1.tscn")
