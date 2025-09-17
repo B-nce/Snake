b4edb692d9c82bcb170813e2845bd2ff4a2ab4ed
Tihamér Szabó <w8qbke@inf.elte.hu>
2025-09-17 18:16:31 +0200

    Added snake skin selector to options

--- snake/scenes/options_scene/options.gd
+++ snake/scenes/options_scene/options.gd
@@ -16,3 +16,13 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_resolutions_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
